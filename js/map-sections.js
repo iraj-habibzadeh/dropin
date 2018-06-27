0dfd872194a463c3349d00a961d972850274b990
iraj.habibzadeh <38947112+iraj-habibzadeh@users.noreply.github.com>
2018-06-27 12:04:56 +0430

    version 1
    not responsive

---
+++ js/map-sections.js
@@ -0,0 +1,254 @@
+function place() {
+    document.getElementById("content").innerHTML =
+        "<div class=\"row\">" +
+        "                                <div class=\"col-5 place-image\">" +
+        "                                    <img src=\"../img/bitmap%20(30).png\" class=\"img-fluid\">" +
+        "                                </div>" +
+        "                                <div class=\"col-7 places-text text-right p-0\">" +
+        "                                    <h6>سفره صبحانه</h6>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/food.png\">" +
+        "" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                            دسته بندی غذا و رستوران" +
+        "" +
+        "                                       </span>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/address.png\">" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                           آدرس : تهران، منطقه ۹ نرسیده به شهرد" +
+        "داری، جنب داروخانه اطبا" +
+        "                                       </span>" +
+        "                                    </p>" +
+        "                                    </p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 place-image\">" +
+        "                                    <img src=\"../img/bitmap%20(25).png\" class=\"img-fluid\">" +
+        "                                </div>" +
+        "                                <div class=\"col-7 places-text text-right p-0\">" +
+        "                                    <h6>رستوران ملل طبیعت</h6>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/food.png\">" +
+        "" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                            دسته بندی غذا و رستوران" +
+        "" +
+        "                                       </span>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/address.png\">" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                           آدرس : تهران، منطقه ۹ نرسیده به شهرد" +
+        "داری، جنب داروخانه اطبا" +
+        "                                       </span>" +
+        "                                    </p>" +
+        "                                    </p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 place-image\">" +
+        "                                    <img src=\"../img/bitmap%20(23).png\" class=\"img-fluid\">" +
+        "                                </div>" +
+        "                                <div class=\"col-7 places-text text-right p-0\">" +
+        "                                    <h6>رستوران اردک آبی</h6>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/food.png\">" +
+        "" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                            دسته بندی غذا و رستوران" +
+        "" +
+        "                                       </span>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/address.png\">" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                           آدرس : تهران، منطقه ۹ نرسیده به شهرد" +
+        "داری، جنب داروخانه اطبا" +
+        "                                       </span>" +
+        "                                    </p>" +
+        "                                    </p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 place-image\">" +
+        "                                    <img src=\"../img/bitmap%20(21).png\" class=\"img-fluid\">" +
+        "                                </div>" +
+        "                                <div class=\"col-7 places-text text-right p-0\">" +
+        "                                    <h6>موبی‌دیک</h6>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/food.png\">" +
+        "" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                            دسته بندی غذا و رستوران" +
+        "" +
+        "                                       </span>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/address.png\">" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                           آدرس : تهران، منطقه ۹ نرسیده به شهرد" +
+        "داری، جنب داروخانه اطبا" +
+        "                                       </span>" +
+        "                                    </p>" +
+        "                                    </p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 place-image\">" +
+        "                                    <img src=\"../img/bitmap%20(20).png\" class=\"img-fluid\">" +
+        "                                </div>" +
+        "                                <div class=\"col-7 places-text text-right p-0\">" +
+        "                                    <h6>باغ رستوران شاندیز</h6>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/food.png\">" +
+        "" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                            دسته بندی غذا و رستوران" +
+        "" +
+        "                                       </span>" +
+        "                                    <p>" +
+        "                                       <span>" +
+        "                                           <img src=\"../img/address.png\">" +
+        "                                       </span>" +
+        "                                        <span>" +
+        "                                           آدرس : تهران، منطقه ۹ نرسیده به شهرد" +
+        "داری، جنب داروخانه اطبا" +
+        "                                       </span>" +
+        "                                    </p>" +
+        "                                    </p>" +
+        "                                </div>" +
+        "                            </div>"
+}
+
+function users() {
+    document.getElementById("content").innerHTML =
+        "<div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(4).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>شرف‌الملوک </h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button \">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(5).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>علی راشدی</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(6).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>محمدعارف ظریف</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(7).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>بهمن قلینواز</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>"
+}
+
+function events() {
+    document.getElementById("content").innerHTML =
+        "<div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(4).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>شرف‌الملوک </h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button \">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(5).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>علی راشدی</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(6).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>محمدعارف ظریف</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>" +
+        "                            <div class=\"media\">" +
+        "                                <img class=\"align-self-center user-img \" src=\"../img/bitmap%20(7).png\">" +
+        "                                <div class=\"media-body col-7 text-right pt-4\">" +
+        "                                    <h6>بهمن قلینواز</h6>" +
+        "                                    <p>@ziaowsky</p>" +
+        "                                </div>" +
+        "                                <div class=\"col-5 submit-button\">" +
+        "                                    <button class=\"btn-primary\"><span><img" +
+        "                                            src=\"../img/add-user-male.png\"></span>دنبال" +
+        "                                        کردن" +
+        "                                    </button>" +
+        "                                </div>" +
+        "                            </div>"
+}
+
+function exit() {
+    document.getElementById("search").style.display = "none";
+    document.getElementById("main").classList.add('col-12');
+
+    document.getElementById("main").classList.remove('col-9');
+
+    if (document.getElementById("main").classList.contains('col-12'))
+
+        document.getElementById("main").classList.toggle('col-9');
+
+}
